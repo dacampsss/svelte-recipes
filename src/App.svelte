91db410cc5a4898385c612b4c835c02c6e1fe45b
Daltro Augusto <daltroaugusto@tutanota.com>
2024-11-30 23:42:30 -0300

    routing and an ugly start page

--- src/App.svelte
+++ src/App.svelte
@@ -1,56 +1,33 @@
 <script>
-  import { register, init, getLocaleFromNavigator, _, isLoading } from 'svelte-i18n';
-  import Recipe from './Recipe.component.svelte';
+  import { register, init, getLocaleFromNavigator, isLoading } from 'svelte-i18n';
+  import Router from 'svelte-spa-router';
+
+  import Index from './screens/index.svelte';
+  import Omelette from './screens/omelette.svelte';
+  import Create from './screens/create.svelte';
+  import Source from './screens/source.svelte';
+
+  const routes = {
+    '/': Index,
+    '/example': Omelette,
+    '/createRecipe': Create,
+    '/source': Source,
+  };
 
   register('en', () => import('./locales/en.json'));
   register('pt', () => import('./locales/pt.json'));
 
   init({
     fallbackLocale: 'en',
-    initialLocale: getLocaleFromNavigator(),
+    initialLocale: 'en' || getLocaleFromNavigator(),
   });
-
-  const omeletteNutrition = new Map([
-    ['Calories', '277kcal'],
-    ['Carbs', '0g'],
-    ['Protein', '20g'],
-    ['Fat', '22g'],
-  ]);
-
-  let props = {
-    title: 'Simple Omelette Recipe',
-    description:
-      'An easy and quick dish, perfect for any meal. This classic omelette combines beaten eggs cooked to perfection, optionally filled with your choice of cheese, vegetables, or meats.',
-    imgUrl: '/img/image-omelette.jpeg',
-    preparationTime: {
-      total: 'Approximately 10 minutes',
-      preparation: '5 minutes',
-      cooking: '5 minutes',
-    },
-    ingredients: [
-      '2-3 large eggs',
-      'Salt, to taste',
-      'Pepper, to taste',
-      '1 tablespoon of butter or oil',
-      'Optional fillings: cheese, diced vegetables, cooked meats, herbs',
-    ],
-    instructions: [
-      '<b>Beat the eggs:</b> In a bowl, beat the eggs with a pinch of salt and pepper until they are well mixed. You can add a tablespoon of water or milk for a fluffier texture.',
-      '<b>Heat the pan:</b> Place a non-stick frying pan over medium heat and add butter or oil.',
-      '<b>Cook the omelette:</b> Once the butter is melted and bubbling, pour in the eggs. Tilt the pan to ensure the eggs evenly coat the surface.',
-      '<b>Add fillings (optional):</b> When the eggs begin to set at the edges but are still slightly runny in the middle, sprinkle your chosen fillings over one half of the omelette.',
-      '<b>Fold and serve:</b> As the omelette continues to cook, carefully lift one edge and fold it over the fillings. Let it cook for another minute, then slide it onto a plate.',
-      '<b>Enjoy:</b> Serve hot, with additional salt and pepper if needed.',
-    ],
-    nutrition: omeletteNutrition
-  };
 </script>
 
 <main>
   {#if $isLoading}
     <span>carregando</span>
   {:else}
-    <Recipe {...props} />
+    <Router {routes} />
   {/if}
 </main>
 
---
+++ src/screens/index.svelte
@@ -0,0 +1,34 @@
+<script>
+  import { _ } from "svelte-i18n";
+  import { push } from 'svelte-spa-router';
+</script>
+
+<main>
+  <h1>{$_('index.welcome')}</h1>
+  <p class="tagline">
+    {$_('index.description')}
+  </p>
+
+  <button onclick={() => push('/createRecipe')} class="create">
+    {$_('index.btn1')}
+  </button>
+  <button onclick={() => push('/example')} class="example">
+    {$_('index.btn2')}
+  </button>
+  <button onclick={() => push('/source')} class="source">
+    {$_('index.btn3')}
+  </button>
+
+  <footer>
+    <span class="license-notice">
+      <img src="/img/agpl3-88.png" alt="GNU Affero GPL version 3 license logo" />
+
+      This work utilizes the GNU Affero GPLv3 and therefore you need to utilize this same license in order to reuse or republish this work in any
+      forms. Click here to read the full license.
+    </span>
+  </footer>
+</main>
+
+<style lang="scss">
+
+</style>
